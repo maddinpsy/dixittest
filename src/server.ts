897b9b31ec339f7f4545a30d151ee25e0f9b1d6e
Martin Hoffmann <maddinpsycho@gmail.com>
2021-03-02 13:53:09 +0100

    fix run with esm

--- src/server.ts
+++ src/server.ts
@@ -9,14 +9,7 @@ const PORT = Number(process.env.PORT || 8000);
 const server = Server({ games: [Dixit] });
 
 // Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, './build');
+const frontEndAppBuildPath = path.resolve(__dirname, '.');
 server.app.use(serve(frontEndAppBuildPath))
 
-server.run(PORT, () => {
-  server.app.use(
-    async (ctx, next) => await serve(frontEndAppBuildPath)(
-      Object.assign(ctx, { path: 'index.html' }),
-      next
-    )
-  )
-});
+server.run(PORT);
