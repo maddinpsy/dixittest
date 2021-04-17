e716a83d66c2f7bd5ca730e4d6e78647441a4873
Martin Hoffmann <maddinpsycho@gmail.com>
2021-04-17 16:38:46 +0200

    enlarge on click

--- README.md
+++ README.md
@@ -2,11 +2,8 @@
 implementation of boardgame dixit in node.js. Hosted on heroku.
 
 # known bugs
-- flicker at startup
-
 
 # planned features
-- enlarge images on click
 
 # start developing 
 0. install node and npm
@@ -18,6 +15,8 @@ implementation of boardgame dixit in node.js. Hosted on heroku.
 # deploy
 1. npm run build
 2. start server
+
   a) heroku local
+
   b) npm run serve
 
--- src/components/Board/Cards/index.tsx
+++ src/components/Board/Cards/index.tsx
@@ -6,19 +6,42 @@ import imageLoader from 'images';
 import { FullPlayerInfo } from "components/Board"
 import { Button } from "components/Button"
 import style from "./style.module.scss";
+import classNames from "classnames";
+import { render } from "react-dom";
 
 
-export function Cards(props: { cards: number[] }) {
-    const list = props.cards.map((value, idx) => (
-        <div key={idx} className={style.Card__container}>
-            <img src={imageLoader()[value]} alt={"CardID:" + value} className={style.Card__image} />
-        </div>
-    ))
-    return (
-        <div className={style.Card__list}>
-            {list}
-        </div>
-    )
+export class Cards extends React.Component<{ cards: number[] }, { fullScreenCard?: number }>{
+    constructor(props: any) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        const list = this.props.cards.map((value, idx) => (
+            <div key={idx} className={style.Card__container}>
+                <img
+                    src={imageLoader()[value]}
+                    alt={"CardID:" + value}
+                    className={style.Card__image}
+                    onClick={() => this.setState({ fullScreenCard: value })}
+                />
+            </div>
+        ))
+        return (
+            <div className={style.Card__list} >
+                { list}
+                {this.state.fullScreenCard ?
+                    <div className={style.Card__fullscreenContainer} onClick={() => this.setState({ fullScreenCard: undefined })}>
+                        <img
+                            src={imageLoader()[this.state.fullScreenCard]}
+                            alt={"CardID:" + this.state.fullScreenCard}
+                            className={style.Card__image} />
+                    </div>
+                    :
+                    ""
+                }
+            </div>
+        )
+    }
 }
 
 export interface CardsToChooseProps {
@@ -27,11 +50,12 @@ export interface CardsToChooseProps {
     animationDestination?: React.RefObject<HTMLDivElement>
 }
 
-export class CardsToChoose extends React.Component<CardsToChooseProps, { selectedCard?: number }>
+export class CardsToChoose extends React.Component<CardsToChooseProps, { fullScreenCard?: number }>
 {
     imgRefs: { cardID: number, ref: React.RefObject<HTMLImageElement> }[];
     constructor(props: any) {
         super(props);
+        this.state = {};
         this.imgRefs = this.props.cards.map(cardID => { return { cardID: cardID, ref: React.createRef() } });
     }
 
@@ -62,11 +86,11 @@ export class CardsToChoose extends React.Component<CardsToChooseProps, { selecte
                 }
             }, 10);
             //after animation
-            n.addEventListener("transitionend",()=>{
+            n.addEventListener("transitionend", () => {
                 n.remove();
                 this.props.handler(cardId)
             });
-            n.addEventListener("transitioncancel",()=>{
+            n.addEventListener("transitioncancel", () => {
                 n.remove();
                 this.props.handler(cardId)
             });
@@ -75,12 +99,19 @@ export class CardsToChoose extends React.Component<CardsToChooseProps, { selecte
             this.props.handler(cardId);
         }
     }
-
+    setFullScreenCard(cardID?: number) {
+        this.setState({ fullScreenCard: cardID })
+    }
     render() {
         const list = this.props.cards.map((value, idx) => {
             return (
                 <div key={idx} className={style.Card__container}>
-                    <img ref={this.imgRefs.find(o => o.cardID === value)?.ref} src={imageLoader()[value]} alt={"CardID:" + value} className={style.Card__image} />
+                    <img
+                        ref={this.imgRefs.find(o => o.cardID === value)?.ref}
+                        src={imageLoader()[value]} alt={"CardID:" + value}
+                        className={style.Card__image}
+                        onClick={() => this.setFullScreenCard(value)}
+                    />
                     <Button onClick={() => this.onSelectCard(value)} theme="orange" size="small" >Choose this</Button>
                 </div>
             )
@@ -89,13 +120,27 @@ export class CardsToChoose extends React.Component<CardsToChooseProps, { selecte
         return (
             <div className={style.Card__list}>
                 {list}
+                {this.state.fullScreenCard ?
+                    <div className={style.Card__fullscreenContainer} onClick={() => this.setFullScreenCard()}>
+                        <img
+                            src={imageLoader()[this.state.fullScreenCard]}
+                            alt={"CardID:" + this.state.fullScreenCard}
+                            className={style.Card__image} />
+                    </div>
+                    :
+                    ""
+                }
             </div>
         )
     }
 }
 
 
-export class CardsFullInfo extends React.Component<{ playedCards: PlayedCard[], playerInfo: FullPlayerInfo , forwardedRef?: React.RefObject<HTMLDivElement>;}> {
+export class CardsFullInfo extends React.Component<{ playedCards: PlayedCard[], playerInfo: FullPlayerInfo, forwardedRef?: React.RefObject<HTMLDivElement>; }, { fullScreenCard?: number }> {
+    constructor(props: any) {
+        super(props)
+        this.state = {}
+    }
     mapToName(playerID?: string): string {
         const name = this.props.playerInfo.find(p => p.playerID === playerID)?.nickname;
         if (name === undefined) {
@@ -119,13 +164,28 @@ export class CardsFullInfo extends React.Component<{ playedCards: PlayedCard[], 
         const list = this.props.playedCards.map((value, idx) => (
             <div key={idx} className={style.Card__container}>
                 <div>Played by: {this.mapToName(value.playedBy)}</div>
-                <img src={value.cardID ? imageLoader()[value.cardID] : backside} alt={"CardID:" + value.cardID} className={style.Card__image} />
+                <img
+                    src={value.cardID ? imageLoader()[value.cardID] : backside}
+                    alt={"CardID:" + value.cardID}
+                    className={style.Card__image}
+                    onClick={() => this.setState({ fullScreenCard: value.cardID })}
+                />
                 {this.voters(value.votedBy)}
             </div>
         ))
         return (
             <div ref={this.props.forwardedRef} className={style.Card__list}>
                 {list}
+                {this.state.fullScreenCard ?
+                    <div className={style.Card__fullscreenContainer} onClick={() => this.setState({ fullScreenCard: undefined })}>
+                        <img
+                            src={imageLoader()[this.state.fullScreenCard]}
+                            alt={"CardID:" + this.state.fullScreenCard}
+                            className={style.Card__image} />
+                    </div>
+                    :
+                    ""
+                }
             </div>
         )
     }
